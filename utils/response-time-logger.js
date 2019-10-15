c613ec0728c6c581429271e7e3b60379324c7c28
Scott Norris <snorris@snorris-a01.vmware.com>
2019-10-15 20:41:18 +1100

    change wavefront source name to be dynamic

--- utils/response-time-logger.js
+++ utils/response-time-logger.js
@@ -5,7 +5,7 @@ const DEPLOYMENTID = process.env.DEPLOYMENT_ID || "vra.node.testing.new";
 const WAVEFRONT_TOKEN = process.env.WAVEFRONT_TOKEN
 const prefix = DEPLOYMENTID +".nodejs.geocode";
 
-const directReporter = new metrics.WavefrontDirectReporter(registry, prefix,  "surf.wavefront.com", WAVEFRONT_TOKEN, { 'app': "vra.app.weather", 'source': DEPLOYMENTID});
+const directReporter = new metrics.WavefrontDirectReporter(registry, prefix,  "surf.wavefront.com", WAVEFRONT_TOKEN, { 'app': "vra.app.weather", 'source': DEPLOYMENTID+"-geocode"});
 directReporter.start(5000);
 const c = new metrics.Counter();
 registry.addTaggedMetric("request.time", c, {"vra.service":"geocode"});
