4774d05300cb64231d4010118b34aba6ad18600c
Scott Norris <snorris@snorris-a01.vmware.com>
2019-10-15 14:16:14 +1100

    added node js wavefront integration

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const path = require('path')
 const express = require('express')
 const geocode = require('./utils/geocode')
 const app = express();
-
+const logResponseTime = require("./utils/response-time-logger");
 const PORT = process.env.WEATHER_GEOCODE_LB_SERVICE_PORT || 3001
 
+app.use(logResponseTime)
 app.get('/api', (req, res) => {
     if (!req.query.address){
         return res.send({
